a5b0fc7b411da4ddfbb4cf033f472c7e73a8c7fb
alison de rozado batista <alisonrbjs@gmail.com>
2023-10-06 20:48:20 -0300

    novo sql

--- src/backend/sql/sql.sql
+++ src/backend/sql/sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28/09/2023 às 09:48
+-- Tempo de geração: 07/10/2023 às 01:47
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `ihm`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `tipo` varchar(40) NOT NULL,
+  `id_ref` int(11) NOT NULL,
+  `resposta` tinyint(1) NOT NULL,
+  `texto` varchar(2000) NOT NULL,
+  `id_resposta` int(11) NOT NULL,
+  `tempo` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `user`, `tipo`, `id_ref`, `resposta`, `texto`, `id_resposta`, `tempo`) VALUES
+(28, 52, 'livro', 69, 0, 'sim sou eu', 0, '2023-10-06'),
+(29, 52, 'livro', 69, 1, 'mas abe ela que eu escuto', 28, '2023-10-06'),
+(32, 52, 'livro', 69, 1, 'mas sabe ela que eu sei que ela me escuta', 29, '2023-10-06'),
+(33, 52, 'livro', 69, 1, 'mal sabe ela que eu sei que ela sabe que eu sei que ela sabe que eu escuto', 32, '2023-10-06'),
+(34, 52, 'livro', 69, 0, 'oi gente', 0, '2023-10-06'),
+(35, 50, 'livro', 69, 1, 'mal sabe el... droga me perdi', 33, '2023-10-06'),
+(36, 50, 'livro', 69, 1, 'oiee amorrr', 34, '2023-10-06'),
+(37, 50, 'livro', 69, 1, 'sim beaa', 29, '2023-10-06');
 
 -- --------------------------------------------------------
 
@@ -85,22 +116,23 @@ CREATE TABLE `livro_publi` (
   `sinopse` varchar(1000) NOT NULL,
   `classificacao` varchar(50) NOT NULL,
   `publico` tinyint(1) NOT NULL,
-  `finalizado` tinyint(1) NOT NULL
+  `finalizado` tinyint(1) NOT NULL,
+  `tema` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `livro_publi`
 --
 
-INSERT INTO `livro_publi` (`id`, `user_id`, `nome`, `imagem`, `tempo`, `genero`, `texto`, `sinopse`, `classificacao`, `publico`, `finalizado`) VALUES
-(68, 50, 'steven universo fake', '50_1694899893.jpeg', '2023-09-16', '[\"0\",\"1\",\"3\"]', '[]', '<p>Sinopse:\nEm um futuro onde a realidade virtual é a norma, uma falha no sistema traz personagens do desenho \"Steven Universo\" para o mundo real. Alice, uma fã da série, se encontra com seus heróis fictícios, mas percebe que algo está errado. Ela precisa unir forças com eles para enfrentar desafios e segredos sombrios que ameaçam ambos os mundos. \"Steven Universo Fake\" é uma emocionante aventura que questiona o que é real e o poder da amizade.</p>', 'dezoito', 0, 0),
-(69, 50, 'urinol', '50_1694899905.jpeg', '2023-09-28', '[\"5\",\"8\"]', '[]', '<p>Este livro explora as experiências e reflexões humanas que ocorrem nos banheiros públicos. De forma inesperada e humorística, ele revela as histórias, pensamentos e situações engraçadas que todos nós enfrentamos nesses espaços compartilhados. Uma leitura divertida que nos faz enxergar o cotidiano de uma maneira única.</p>', 'dez', 0, 0),
-(70, 50, 'alison amarelo', '50_1694899923.jpeg', '2023-09-16', '[\"7\",\"10\",\"14\"]', '', '<p>Neste romance emocionante, Alison, uma jovem de espírito livre com cabelos amarelos brilhantes, parte em uma jornada de autodescoberta e aventura. Em um mundo onde a conformidade é a norma, ela se destaca como um raio de sol. Ao seguir seu coração e suas paixões, Alison desencadeia uma série de eventos que a levam a lugares inesperados e a pessoas que mudarão sua vida para sempre. \"Alison Amarelo\" é uma história inspiradora sobre coragem, autenticidade e a busca pelo verdadeiro significado da vida.</p>', '', 0, 0),
-(71, 50, 'miguelias', '50_1694899997.jpeg', '2023-09-28', '[\"8\",\"17\"]', '', '<p>é uma história de superação e determinação que segue a jornada de um jovem chamado Miguel. Criado em um bairro difícil, Miguel enfrenta desafios extraordinários em sua busca por um futuro melhor. Inspirado por seu amor pela música e um antigo mistério de família, ele embarca em uma emocionante aventura que o leva a descobertas surpreendentes sobre seu passado e a verdade sobre o poder da perseverança. Miguelhias é uma história cativante sobre sonhos, família e a busca pela identidade.</p>', '', 0, 0),
-(73, 50, 'cachorro do bruno', '50_1694901075.jpeg', '2023-09-16', '[\"0\",\"2\",\"3\"]', '', '<p>A história de Max, o cachorro do Bruno, é uma aventura repleta de amizade e diversão. Juntos, eles enfrentam desafios e fazem descobertas emocionantes enquanto exploram o mundo à sua maneira única. Uma história cativante sobre a ligação especial entre um homem e seu melhor amigo de quatro patas.</p>', '', 0, 0),
-(74, 50, 'diario de um banana', '50_1695762643.PNG', '2023-09-26', '[\"2\",\"3\",\"8\"]', '', '<p>Acompanhe um ano na vida de Bruno por meio de seu diário pessoal, onde ele compartilha seus pensamentos, sentimentos e experiências do dia a dia. Uma história simples e sincera sobre as alegrias e desafios de crescer e descobrir quem somos.</p>', '', 0, 0),
-(75, 50, 'diario de um banana vol.2 kkkkkkk', '50_1695762771.jpg', '2023-09-26', '[\"2\",\"3\",\"8\"]', '', '<p style=\"line-height: 2;\"><strong><em>Glow UPPP</em></strong></p>', '', 0, 0),
-(78, 50, 'camelo santo', '50_1695873486.PNG', '2023-09-28', '[\"6\"]', '', '', 'doze', 1, 0);
+INSERT INTO `livro_publi` (`id`, `user_id`, `nome`, `imagem`, `tempo`, `genero`, `texto`, `sinopse`, `classificacao`, `publico`, `finalizado`, `tema`) VALUES
+(68, 50, 'steven universo fake', '50_1694899893.jpeg', '2023-10-03', '[\"1\",\"3\"]', '{\"1\":\"isso\",\"2\":\"gdfgdgf\",\"3\":\"fgdfgfdg\",\"4\":\"opop\",\"5\":\"sai\",\"6\":\"yukyuk\",\"7\":\"ukuyk\",\"8\":\"yukuyku\",\"9\":\"yuiyui\",\"10\":\"capitulo Novo\",\"11\":\"capitulo Novo\",\"12\":\"capitulo Novo\",\"13\":\"capitulo Novo\",\"14\":\"capitulo Novo\",\"15\":\"capitulo Novo\",\"16\":\"capitulo Novo\"}', '<p>esse erro eu tenhyuyuyutytytytytyuyuyouytotyotoutyuo</p>', 'dezoito', 1, 0, '#214b54'),
+(69, 50, 'urinol', '50_1694899905.jpeg', '2023-10-06', '[\"5\",\"8\"]', '{\"1\":\"lindedsdasdasdasdsadsadsd\"}', '<p>Este livro explora as experiências e reflexões humanas que ocorrem nos banheiros públicos. De forma inesperada e humorística, ele revela as histórias, pensamentos e situações engraçadas que todos nós enfrentamos nesses espaços compartilhados. Uma leitura divertida que nos faz enxergar o cotidiano de uma maneira única.</p>', 'dez', 1, 0, '#6c91ec'),
+(70, 50, 'alison amarelo', '50_1694899923.jpeg', '2023-10-05', '[\"7\",\"10\",\"14\"]', '', '<p>Neste romance emocionante, Alison, uma jovem de espírito livre com cabelos amarelos brilhantes, parte em uma jornada de autodescoberta e aventura. Em um mundo onde a conformidade é a norma, ela se destaca como um raio de sol. Ao seguir seu coração e suas paixões, Alison desencadeia uma série de eventos que a levam a lugares inesperados e a pessoas que mudarão sua vida para sempre. \"Alison Amarelo\" é uma história inspiradora sobre coragem, autenticidade e a busca pelo verdadeiro significado da vida.</p>', '', 1, 1, '#087F97'),
+(71, 50, 'miguelias', '50_1694899997.jpeg', '2023-10-06', '[\"8\",\"17\"]', '{\"1\":\"e\",\"2\":\"r\",\"3\":\"cq\"}', '<p>é uma história de superação e determinação que segue a jornada de um jovem chamado Miguel. Criado em um bairro difícil, Miguel enfrenta desafios extraordinários em sua busca por um futuro melhor. Inspirado por seu amor pela música e um antigo mistério de família, ele embarca em uma emocionante aventura que o leva a descobertas surpreendentes sobre seu passado e a verdade sobre o poder da perseverança. Miguelhias é uma história cativante sobre sonhos, família e a busca pela identidade.</p>', '', 1, 0, '#e6006f'),
+(73, 50, 'cachorro do bruno', '50_1694901075.jpeg', '2023-10-03', '[\"2\",\"3\"]', '[]', '<p>A história de Max, o cachorro do Bruno, é uma aventura repleta de amizade e diversão. Juntos, eles enfrentam desafios e fazem descobertas emocionantes enquanto exploram o mundo à sua maneira única. Uma história cativante sobre a ligação especial entre um homem e seu melhor amigo de quatro patas.</p>', '', 1, 0, '#087F97'),
+(122, 52, 'acido ribunocleico', '52_1696611763.jpg', '2023-10-06', '[\"2\",\"9\",\"13\"]', '', '', 'livre', 1, 0, '#ff0000'),
+(123, 52, 'eu vou tomar um tacaca', '52_1696611831.jpeg', '2023-10-06', '[\"3\",\"7\",\"14\"]', '', '', 'livre', 0, 0, '#1a3736'),
+(124, 52, 'xin', '52_1696611879.jpeg', '2023-10-06', '[\"4\",\"8\",\"17\"]', '', '', 'livre', 1, 0, '#099572');
 
 -- --------------------------------------------------------
 
@@ -122,11 +154,18 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `fotoPerfil`, `genero`) VALUES
-(50, 'bolsetenha', 'alisonkpops@gmail.com', 'minhasenha833', '50_1695886987.png', '[\"1\",\"3\",\"5\",\"6\",\"8\",\"10\",\"13\"]');
+(50, 'bruninho', 'alisonkpops@gmail.com', 'minhasenha833', '50_1695886987.png', '[\"1\",\"3\",\"5\",\"6\",\"8\",\"10\",\"13\"]'),
+(52, 'beatriz', 'alisonkpopss@gmail.com', 'minhasenha833', '52_1696611646.jpg', '[\"3\",\"5\",\"6\",\"7\",\"8\",\"9\",\"11\"]');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `feed_publi`
@@ -159,6 +198,12 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
 -- AUTO_INCREMENT de tabela `feed_publi`
 --
 ALTER TABLE `feed_publi`
@@ -174,13 +219,13 @@ ALTER TABLE `genero`
 -- AUTO_INCREMENT de tabela `livro_publi`
 --
 ALTER TABLE `livro_publi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
