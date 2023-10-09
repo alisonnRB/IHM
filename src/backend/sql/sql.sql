ef91429b52b932ce84021454a2ce556809a5ed83
alison de rozado batista <alisonrbjs@gmail.com>
2023-10-09 15:43:25 -0300

    att

--- src/backend/sql/sql.sql
+++ src/backend/sql/sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/10/2023 às 01:47
+-- Tempo de geração: 09/10/2023 às 20:41
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -35,22 +35,37 @@ CREATE TABLE `comentarios` (
   `resposta` tinyint(1) NOT NULL,
   `texto` varchar(2000) NOT NULL,
   `id_resposta` int(11) NOT NULL,
-  `tempo` date NOT NULL
+  `tempo` date NOT NULL,
+  `conversa` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `comentarios`
 --
 
-INSERT INTO `comentarios` (`id`, `user`, `tipo`, `id_ref`, `resposta`, `texto`, `id_resposta`, `tempo`) VALUES
-(28, 52, 'livro', 69, 0, 'sim sou eu', 0, '2023-10-06'),
-(29, 52, 'livro', 69, 1, 'mas abe ela que eu escuto', 28, '2023-10-06'),
-(32, 52, 'livro', 69, 1, 'mas sabe ela que eu sei que ela me escuta', 29, '2023-10-06'),
-(33, 52, 'livro', 69, 1, 'mal sabe ela que eu sei que ela sabe que eu sei que ela sabe que eu escuto', 32, '2023-10-06'),
-(34, 52, 'livro', 69, 0, 'oi gente', 0, '2023-10-06'),
-(35, 50, 'livro', 69, 1, 'mal sabe el... droga me perdi', 33, '2023-10-06'),
-(36, 50, 'livro', 69, 1, 'oiee amorrr', 34, '2023-10-06'),
-(37, 50, 'livro', 69, 1, 'sim beaa', 29, '2023-10-06');
+INSERT INTO `comentarios` (`id`, `user`, `tipo`, `id_ref`, `resposta`, `texto`, `id_resposta`, `tempo`, `conversa`) VALUES
+(71, 50, 'livro', 69, 0, 'desgraçada', 0, '2023-10-07', 0),
+(72, 50, 'livro', 69, 1, 'mal sabe ela que eu escuto', 71, '2023-10-07', 71),
+(73, 52, 'livro', 69, 1, 'mas sabe ela que eu escuto que ela dix que me escuta', 72, '2023-10-07', 71),
+(74, 52, 'livro', 69, 0, 'doidinha', 72, '2023-10-07', 0),
+(75, 50, 'livro', 69, 1, 'mal sabe ela que eu escuto que ela diz que sabe que me escuta', 73, '2023-10-07', 71),
+(76, 52, 'livro', 69, 1, 'mal sabe ela que eu sei ela diz que escuta quando eu digo escuto o que ela diz', 75, '2023-10-07', 71),
+(77, 52, 'livro', 69, 1, 'vc vai perderrrr', 71, '2023-10-07', 71),
+(78, 50, 'livro', 69, 1, 'mal sabe ela que e... droga me perdi AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 76, '2023-10-07', 71),
+(79, 50, 'livro', 69, 1, 'vc né bem', 74, '2023-10-07', 74),
+(80, 50, 'livro', 125, 0, 'foda', 0, '2023-10-07', 0),
+(81, 50, 'livro', 124, 0, 'vaca', 0, '2023-10-07', 0),
+(82, 50, 'livro', 70, 0, 'oi', 0, '2023-10-08', 0),
+(83, 50, 'livro', 70, 0, 'asas', 0, '2023-10-08', 0),
+(84, 50, 'livro', 70, 0, 'asas', 0, '2023-10-08', 0),
+(85, 50, 'livro', 70, 1, 'asasa', 82, '2023-10-08', 82),
+(86, 50, 'livro', 70, 1, 'fmdkm', 82, '2023-10-08', 82),
+(87, 50, 'livro', 70, 1, 'dwsdw', 86, '2023-10-08', 82),
+(88, 50, 'livro', 70, 1, 'dwsdw', 86, '2023-10-08', 82),
+(89, 52, 'livro', 70, 1, 'oiio', 85, '2023-10-08', 82),
+(90, 52, 'livro', 70, 1, 'dasdsa', 88, '2023-10-08', 82),
+(91, 52, 'livro', 70, 0, 'wsdd', 0, '2023-10-08', 0),
+(92, 52, 'livro', 70, 1, 'sim mana', 83, '2023-10-08', 83);
 
 -- --------------------------------------------------------
 
@@ -113,26 +128,28 @@ CREATE TABLE `livro_publi` (
   `tempo` date NOT NULL,
   `genero` varchar(45) NOT NULL,
   `texto` varchar(300) NOT NULL,
+  `pronto` varchar(1000) NOT NULL,
   `sinopse` varchar(1000) NOT NULL,
   `classificacao` varchar(50) NOT NULL,
   `publico` tinyint(1) NOT NULL,
   `finalizado` tinyint(1) NOT NULL,
-  `tema` varchar(50) NOT NULL
+  `tema` varchar(50) NOT NULL,
+  `tags` varchar(2000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `livro_publi`
 --
 
-INSERT INTO `livro_publi` (`id`, `user_id`, `nome`, `imagem`, `tempo`, `genero`, `texto`, `sinopse`, `classificacao`, `publico`, `finalizado`, `tema`) VALUES
-(68, 50, 'steven universo fake', '50_1694899893.jpeg', '2023-10-03', '[\"1\",\"3\"]', '{\"1\":\"isso\",\"2\":\"gdfgdgf\",\"3\":\"fgdfgfdg\",\"4\":\"opop\",\"5\":\"sai\",\"6\":\"yukyuk\",\"7\":\"ukuyk\",\"8\":\"yukuyku\",\"9\":\"yuiyui\",\"10\":\"capitulo Novo\",\"11\":\"capitulo Novo\",\"12\":\"capitulo Novo\",\"13\":\"capitulo Novo\",\"14\":\"capitulo Novo\",\"15\":\"capitulo Novo\",\"16\":\"capitulo Novo\"}', '<p>esse erro eu tenhyuyuyutytytytytyuyuyouytotyotoutyuo</p>', 'dezoito', 1, 0, '#214b54'),
-(69, 50, 'urinol', '50_1694899905.jpeg', '2023-10-06', '[\"5\",\"8\"]', '{\"1\":\"lindedsdasdasdasdsadsadsd\"}', '<p>Este livro explora as experiências e reflexões humanas que ocorrem nos banheiros públicos. De forma inesperada e humorística, ele revela as histórias, pensamentos e situações engraçadas que todos nós enfrentamos nesses espaços compartilhados. Uma leitura divertida que nos faz enxergar o cotidiano de uma maneira única.</p>', 'dez', 1, 0, '#6c91ec'),
-(70, 50, 'alison amarelo', '50_1694899923.jpeg', '2023-10-05', '[\"7\",\"10\",\"14\"]', '', '<p>Neste romance emocionante, Alison, uma jovem de espírito livre com cabelos amarelos brilhantes, parte em uma jornada de autodescoberta e aventura. Em um mundo onde a conformidade é a norma, ela se destaca como um raio de sol. Ao seguir seu coração e suas paixões, Alison desencadeia uma série de eventos que a levam a lugares inesperados e a pessoas que mudarão sua vida para sempre. \"Alison Amarelo\" é uma história inspiradora sobre coragem, autenticidade e a busca pelo verdadeiro significado da vida.</p>', '', 1, 1, '#087F97'),
-(71, 50, 'miguelias', '50_1694899997.jpeg', '2023-10-06', '[\"8\",\"17\"]', '{\"1\":\"e\",\"2\":\"r\",\"3\":\"cq\"}', '<p>é uma história de superação e determinação que segue a jornada de um jovem chamado Miguel. Criado em um bairro difícil, Miguel enfrenta desafios extraordinários em sua busca por um futuro melhor. Inspirado por seu amor pela música e um antigo mistério de família, ele embarca em uma emocionante aventura que o leva a descobertas surpreendentes sobre seu passado e a verdade sobre o poder da perseverança. Miguelhias é uma história cativante sobre sonhos, família e a busca pela identidade.</p>', '', 1, 0, '#e6006f'),
-(73, 50, 'cachorro do bruno', '50_1694901075.jpeg', '2023-10-03', '[\"2\",\"3\"]', '[]', '<p>A história de Max, o cachorro do Bruno, é uma aventura repleta de amizade e diversão. Juntos, eles enfrentam desafios e fazem descobertas emocionantes enquanto exploram o mundo à sua maneira única. Uma história cativante sobre a ligação especial entre um homem e seu melhor amigo de quatro patas.</p>', '', 1, 0, '#087F97'),
-(122, 52, 'acido ribunocleico', '52_1696611763.jpg', '2023-10-06', '[\"2\",\"9\",\"13\"]', '', '', 'livre', 1, 0, '#ff0000'),
-(123, 52, 'eu vou tomar um tacaca', '52_1696611831.jpeg', '2023-10-06', '[\"3\",\"7\",\"14\"]', '', '', 'livre', 0, 0, '#1a3736'),
-(124, 52, 'xin', '52_1696611879.jpeg', '2023-10-06', '[\"4\",\"8\",\"17\"]', '', '', 'livre', 1, 0, '#099572');
+INSERT INTO `livro_publi` (`id`, `user_id`, `nome`, `imagem`, `tempo`, `genero`, `texto`, `pronto`, `sinopse`, `classificacao`, `publico`, `finalizado`, `tema`, `tags`) VALUES
+(68, 50, 'steven universo fake', '50_1694899893.jpeg', '2023-10-08', '[\"1\",\"3\"]', '{\"1\":\"qw\",\"2\":\"hdh\",\"3\":\"dfhfh\"}', '{\"1\":1,\"2\":1,\"3\":0}', '<p>qwqw</p>', 'dezoito', 1, 0, '#214b54', '{\"0\":\"bruno\",\"1\":\"\",\"2\":\"\",\"3\":\"\",\"4\":\"\",\"5\":\"\",\"6\":\"\",\"7\":\"\",\"8\":\"\",\"9\":\"\",\"10\":\"\"}'),
+(70, 50, 'alison amarelo', '50_1694899923.jpeg', '2023-10-05', '[\"7\",\"10\",\"14\"]', '', '', '<p>Neste romance emocionante, Alison, uma jovem de espírito livre com cabelos amarelos brilhantes, parte em uma jornada de autodescoberta e aventura. Em um mundo onde a conformidade é a norma, ela se destaca como um raio de sol. Ao seguir seu coração e suas paixões, Alison desencadeia uma série de eventos que a levam a lugares inesperados e a pessoas que mudarão sua vida para sempre. \"Alison Amarelo\" é uma história inspiradora sobre coragem, autenticidade e a busca pelo verdadeiro significado da vida.</p>', '', 1, 1, '#087F97', ''),
+(71, 50, 'miguelias', '50_1694899997.jpeg', '2023-10-06', '[\"8\",\"17\"]', '', '', '<p>é uma história de superação e determinação que segue a jornada de um jovem chamado Miguel. Criado em um bairro difícil, Miguel enfrenta desafios extraordinários em sua busca por um futuro melhor. Inspirado por seu amor pela música e um antigo mistério de família, ele embarca em uma emocionante aventura que o leva a descobertas surpreendentes sobre seu passado e a verdade sobre o poder da perseverança. Miguelhias é uma história cativante sobre sonhos, família e a busca pela identidade.</p>', '', 1, 0, '#e6006f', ''),
+(73, 50, 'cachorro do bruno', '50_1694901075.jpeg', '2023-10-03', '[\"2\",\"3\"]', '[]', '', '<p>A história de Max, o cachorro do Bruno, é uma aventura repleta de amizade e diversão. Juntos, eles enfrentam desafios e fazem descobertas emocionantes enquanto exploram o mundo à sua maneira única. Uma história cativante sobre a ligação especial entre um homem e seu melhor amigo de quatro patas.</p>', '', 1, 0, '#087F97', ''),
+(122, 52, 'acido ribunocleico', '52_1696611763.jpg', '2023-10-08', '[\"2\",\"9\",\"13\"]', '{\"1\":\"capitulo Novo\",\"2\":\"capitulo Novo\"}', '{\"1\":1,\"2\":0}', '', 'livre', 1, 0, '#bc348f', '{\"0\":\"\",\"1\":\"\",\"2\":\"\",\"3\":\"\",\"4\":\"\",\"5\":\"\",\"6\":\"\",\"7\":\"\",\"8\":\"\",\"9\":\"\",\"10\":\"\"}'),
+(123, 52, 'eu vou tomar um tacaca', '52_1696611831.jpeg', '2023-10-06', '[\"3\",\"7\",\"14\"]', '', '', '', 'livre', 0, 0, '#1a3736', ''),
+(124, 52, 'xin', '52_1696611879.jpeg', '2023-10-06', '[\"4\",\"8\",\"17\"]', '', '', '', 'livre', 1, 0, '#099572', ''),
+(125, 50, 'urinol', '50_1696699628.jpeg', '2023-10-07', '[\"2\",\"4\",\"15\"]', '{\"1\":\"\\u00e9 sobre isso\"}', '{\"1\":1}', '<p>Este livro explora as experiências e reflexões humanas que ocorrem nos banheiros públicos. De forma inesperada e humorística, ele revela as histórias, pensamentos e situações engraçadas que todos nós enfrentamos nesses espaços compartilhados. Uma leitura divertida que nos faz enxergar o cotidiano de uma maneira única.</p>', 'livre', 1, 0, '#efae1f', '');
 
 -- --------------------------------------------------------
 
@@ -201,7 +218,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT de tabela `feed_publi`
@@ -219,7 +236,7 @@ ALTER TABLE `genero`
 -- AUTO_INCREMENT de tabela `livro_publi`
 --
 ALTER TABLE `livro_publi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
--- src/components/render/busca/busca.jsx
+++ src/components/render/busca/busca.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import { useState, useEffect } from "react";
 import './busca.css';
 
-export default function Busca() {
 
+export default function Busca() {
+    const [pesquisa, setPesquisa] = useState('');
 
     return (
-        <div>oi
+        <div className='TelaBusca'>
+            <span id='titlePerfil'>USUÁRIOS</span>
+            <span className='boxLivros'>
+                <input type='text' id='searchText' placeholder='Buscar' value={pesquisa} onChange={(e) => { setPesquisa(e.target.value); }} />
+                <div id='searchImg'></div>
+            </span>
 
+            <div className="pessoasBOX">
+                <span className="BoxCardPessoas">
+                    <span className="cardPessoa">
+                        <div id="perfilPessoa">
+                            <img src=""/>
+                        </div>
+                        <div className="infosPessoa">
+                            <span className="nomePessoa">JOOZINHO</span>
+                            <span className="catPessoa">
+                                <div className="categoria"><img src=""/></div>
+                                <div className="opSeguir"><p>SEGUIR</p></div>
+                            </span>
+                        </div>
+                    </span>
+                </span>
+            </div>
         </div>
     );
 }
