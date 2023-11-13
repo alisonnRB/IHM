8099eb69a875bb312921f052b599fa49085fc309
alisonnRB <alisonrbjs@gmail.com>
2023-11-13 19:32:58 -0300

    temas prontos para serem aplicados

--- src/components/render/option/option.jsx
+++ src/components/render/option/option.jsx
@@ -4,29 +4,45 @@ import './option.css';
 import sol from '../../../imgs/sol.png';
 import lua from '../../../imgs/lua.png';
 
+import { useState, useEffect } from 'react';
+
 //? componente que comporta as opções
 
 export default function Option() {
+
+  const [tema, setTema] = useState('light');
+
+  useEffect(()=>{
+     qual_tema();
+  },[])
+
+  const qual_tema = () =>{
+     let themes = localStorage.getItem('tema');
+     if(themes){
+        setTema(themes);
+     }
+  }
+
   return (
     <div className='option'>
-      <span id='titlePerfil'>Configurações</span>
+      <span id='titlePerfil'>CONFIGURAÇÕES</span>
 
       <div className='comport'>
 
         <div className="com">
           <label htmlFor="idioma">IDIOMA</label>
           <select name="idioma" id="idioma">
-            <option value="PT" className="idi">Português</option>
-            <option value="EN" className="idi">Iglês</option>
-            <option value="ES" className="idi">Espanhol</option>
+            <option value="PT" className="idi">PORTUGUÊS</option>
+            <option value="EN" className="idi">INGLÊS</option>
+            <option value="ES" className="idi">ESPANHOL</option>
           </select>
         </div>
 
         <div className="com">
           <label htmlFor="tema">TEMA</label>
           <span id='tema'>
-            <img src={sol} className="tema" />
-            <img src={lua} className="tema" />
+            <img src={sol} className="tema" onClick={()=>{localStorage.setItem('tema', 'light')}} style={tema === 'light' ? {backgroundColor: '#EBECF0'} : null}/>
+            <img src={lua} className="tema" onClick={()=>{localStorage.setItem('tema', 'dark')}} style={tema === 'dark' ? {backgroundColor: '#EBECF0'} : null}/>
           </span>
         </div>
 
