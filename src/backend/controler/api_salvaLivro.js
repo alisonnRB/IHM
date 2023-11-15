1db3ad296ca0cff3001ff5d7ad21418193ebd6e6
KauaDaviWust <kaua.wust1@gmail.com>
2023-11-15 00:30:37 -0300

    socorro

--- src/backend/controler/api_salvaLivro.js
+++ src/backend/controler/api_salvaLivro.js
@@ -22,7 +22,7 @@ export default {
       body: JSON.stringify(user),
     };
 
-    const response = await fetch('http://192.168.255.193/server/salvaLivros.php', requisição);
+    const response = await fetch('http://10.1.1.211/server/salvaLivros.php', requisição);
 
     const data = await response.json();
     if (data.informacoes == "não autorizado") {
