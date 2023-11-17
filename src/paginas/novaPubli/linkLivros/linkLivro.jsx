28a662707fd68009b890b7bde76cba965008978d
alisonnRB <alisonrbjs@gmail.com>
2023-11-17 03:10:35 -0300

    linkLivro //idioma add

--- src/paginas/novaPubli/linkLivros/linkLivro.jsx
+++ src/paginas/novaPubli/linkLivros/linkLivro.jsx
@@ -7,10 +7,27 @@ import api from "../../../backend/controler/api_search";
 import Card from "./cardLivro/cardLivro";
 import x from "../../../imgs/cancel.png";
 
+import words from './linkLivro.json';
+
 export default function LinkLivro(props) {
     const [livro, setLivro] = useState('');
 
     const [selecionado, setSelecionado] = useState('');
+
+    const [Uword, setUword] = useState('EN');
+
+    useEffect(() => {
+        select_idioma();
+    }, [])
+
+    const select_idioma = () => {
+        let idi = localStorage.getItem('idioma');
+        if (!idi || (idi != 'PT' && idi != 'EN' && idi != 'ES')) {
+            idi = 'EN';
+        }
+        let word = words[idi];
+        setUword(word);
+    }
 
     useEffect(()=>{
         if(selecionado && selecionado.id){
@@ -42,9 +59,9 @@ export default function LinkLivro(props) {
         <div className="BuscaLivros">
             <div className="abaPesquisa">
                 <img id="cancelB" src={x} onClick={()=>{props.setCancel(true)}}/>
-                <span className="titleBox">ANEXAR LIVRO</span>
+                <span className="titleBox">{Uword.anexo}</span>
                 <span className="boxLivros">
-                    <input type='text' id='searchText' placeholder='Buscar' onChange={(e) => {  Pesquisa(e.target.value)}} />
+                    <input type='text' id='searchText' placeholder={Uword.place} onChange={(e) => {  Pesquisa(e.target.value)}} />
                 </span>
 
                 <div className="Buscas">
