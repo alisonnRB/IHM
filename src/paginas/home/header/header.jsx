12e2d7c00db448e9d9c3340493a72bb5b86ba7df
alisonnRB <alisonrbjs@gmail.com>
2023-12-04 20:26:19 -0300

    ajustes

--- src/paginas/home/header/header.jsx
+++ src/paginas/home/header/header.jsx
@@ -8,14 +8,12 @@ function head() {
   //TODO passa como propriedades as informações(user) do usuario para os elementos filhos acessarem
   return (
     <header className='MAIN'>
-        <span className='caixa-left'>
-            <img src={logo} id='logoMAIN'/>
+      <span className='caixa-left'>
+        <img src={logo} id='logoMAIN' />
 
-            <Link to='/login'><button className='btMA'>Login</button></Link>
-            <Link to='/login'><button className='btMA'>Cadastre-se</button></Link>
-        </span>
-
-        <button className='btMA nos'>Sobre nós</button>
+        <Link to='/login'><button className='btMA'>Login</button></Link>
+        <Link to='/login'><button className='btMA'>Cadastre-se</button></Link>
+      </span>
     </header>
   );
 }
